53b27dceddfb3c7644ecb7f3be6a0e7005ab6675
HungSLin <b9980906@gmail.com>
2015-04-10 22:02:02 +0800

    Replace the right one

--- hangman.py
+++ hangman.py
@@ -1,5 +1,135 @@
 import random
 
+HANGMAN=[
+#0
+    """
+
+
+
+
+
+
+
+
+       ───""",
+#1
+    """
+        ┌
+        │       
+        │      
+        │      
+        │      
+        │      
+        │      
+        │      
+        │      
+        │
+       ─┴─""",
+#2
+    """
+        ┌───────
+        │       
+        │      
+        │      
+        │      
+        │      
+        │       
+        │      
+        │      
+        │
+       ─┴─""",
+#3
+    """
+        ┌───────┐
+        │       │
+        │      
+        │      
+        │      
+        │      
+        │       
+        │      
+        │      
+        │
+       ─┴─""",
+#4
+    """
+        ┌───────┐
+        │       │
+        │      ┌┴┐
+        │      │ │
+        │      └┬┘
+        │      
+        │   
+        │      
+        │      
+        │
+       ─┴─""",
+#5
+    """
+        ┌───────┐
+        │       │
+        │      ┌┴┐
+        │      │ │
+        │      └┬┘
+        │      ─┤
+        │       
+        │      
+        │      
+        │
+       ─┴─""",
+#6
+    """
+        ┌───────┐
+        │       │
+        │      ┌┴┐
+        │      │ │
+        │      └┬┘
+        │      ─┼─
+        │       
+        │      
+        │      
+        │
+       ─┴─""",
+#7
+    """
+        ┌───────┐
+        │       │
+        │      ┌┴┐
+        │      │ │
+        │      └┬┘
+        │      ─┼─
+        │       │
+        │      
+        │      
+        │
+       ─┴─""",
+#8
+    """
+        ┌───────┐
+        │       │
+        │      ┌┴┐
+        │      │ │
+        │      └┬┘
+        │      ─┼─
+        │       │
+        │      ┌┘
+        │      │
+        │
+       ─┴─""",
+#9
+    """
+        ┌───────┐
+        │       │
+        │      ┌┴┐
+        │      │ │
+        │      └┬┘
+        │      ─┼─
+        │       │
+        │      ┌┴┐
+        │      │ │
+        │
+       ─┴─"""
+]
 
 
 def random_word_func(answer,print_answer):
@@ -7,63 +137,68 @@ def random_word_func(answer,print_answer):
     random_word = word[random.randint(0,len(word)-1)]
     for x in random_word:
         answer.append(x)
-        print_answer.append("__ ")
+        print_answer.append("__")
     return (answer,print_answer)
 
 def player_guess():
     guess=input("Guess a letter!")
-    if guess in used_letter :
-        pass
-    else:
-        used_letter.append(guess)
     return guess
 
-def guess_judge(guess):
+def guess_judge(guess,print_hangman):
+    print_hangman +=1
     if (guess in answer):
-        print ("You guessed a right letter!")
+        print ("You guess a right letter!")
+        print_hangman -= 1
         for x in range(len(answer)):
             if guess == answer[x]:
                 print_answer[x] = guess
             else:
                 pass
-    elif (guess in print_answer or guess in used_letter):
+    elif (guess in used_letter):
         print("You have guessed this one already")
     else:
         print("Wrong answer!")
+        used_letter.append(guess)
+    return print_hangman
+
+def dis_hangman(print_hangman):
+    print(HANGMAN[print_hangman])
     return
-
-def dis_hangman(tries):
-    HANGMAN
-
-#used_letter_function()
-
-#dis_hangman(tries):
-#    print (HANGMAN[tries-1])
-
-#win_or_not(answer)
-
-
-
-tries=10
+def dis_print_answer(print_answer):
+    dis = ""
+    for i in print_answer:
+        dis += i
+        dis += " "
+    return dis
 
 answer=[]
 print_answer=[]
 used_letter=[]
 
-"""
-main():
-    random_word_func()
-    dis_hangman()
-    player_guess()
-    guess_judge
-"""
 
+def main():
+    print_hangman = 0
+    tries =0
+    random_word_func(answer,print_answer)
+    while tries<=9:
+        guess = player_guess()
+        print_hangman = guess_judge(guess,print_hangman)
+        print ("Ans:"+dis_print_answer(print_answer))
+        dis_hangman(print_hangman)
+        print ("Used letter :"+ str(used_letter))
+        print ("turn " + str(tries))
+        tries += 1
+        print ()
+        if print_answer == answer:
+            print ("You win")
+            break
+        else:
+            if tries == 9:
+                print("You ran out of turns")
+                break
+            else:
+                pass
 
+main()
+        
 
-random_word_func(answer,print_answer)
-guess = player_guess()
-guess_judge(guess)
-print (answer)
-print (print_answer)
-print (used_letter)
-print ("")
